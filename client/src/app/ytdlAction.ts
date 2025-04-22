930364dec87aeb5b75f6c20cdd5b4fd83415b88a
snackkk <tastytothesupreme@gmail.com>
2025-04-21 20:50:09 -0600

    removed the fs.writeFile stuff

--- client/src/app/ytdlAction.ts
+++ client/src/app/ytdlAction.ts
@@ -4,13 +4,7 @@ import * as fs from 'fs';
 
 export async function downloadYTLink(url: string) {
 	if (validateURL(url)) {
-		const download = ytdl(url, { filter: 'audioonly'}).pipe(fs.writeFile('audio.mp3', content, err => {
-			if (err) {
-				console.log(err)
-			} else {
-				console.log("file written successfully")
-			}
-		}), 'audio');
+		const download = ytdl(url, { filter: 'audioonly'});
 		console.log(url);
 	} else {
 		console.log('not a valid url');
