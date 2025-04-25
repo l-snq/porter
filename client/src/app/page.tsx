9a55ab3a8f381ade45de2272994041e1834a6c39
snackkk <tastytothesupreme@gmail.com>
2025-04-24 22:37:57 -0600

    removed pointless files, cleaned up the css and html

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useState} from 'react';
 import { useRouter } from 'next/navigation';
 import { generateFileDownload } from './fileDownload';
+import "./page.css";
 
 export default function Home() {
 	const router = useRouter();
@@ -17,9 +18,11 @@ export default function Home() {
 	}
 
   return (
-    <div>
+    <div id='mainDiv'>
+			<h1>porter.</h1>
+			<h2>convert youtube videos to audio files.</h2>
 			<form onSubmit={handleSubmit}>
-				<div>
+				<div id='formDiv'>
 					<label>youtube link: </label>
 					<input 
 						type='text'
